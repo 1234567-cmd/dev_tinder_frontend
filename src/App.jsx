50fa951a887b22b6a7200ae26ca0fa4a05b5ec4a
waseem ahmad <waseemlone246@gmail.com>
2026-09-12 09:51:24 +0500

    feat: add daisyUI for enhanced styling and update Vite config

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      Hi
-    </>
+    <div className="App">
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
   )
 }
 
