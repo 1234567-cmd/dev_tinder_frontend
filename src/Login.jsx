2dc32bd19c3aaa5d02373790086a0673650944de
waseem ahmad <waseemlone246@gmail.com>
2026-09-13 16:28:48 +0500

    feat: implement Login component with email and password fields

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 export const Login = () => {
+  const [emailId, setEmailId] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = async () => {
+    try{
+      const response = await axios.post('http://localhost:3000/login', {
+        emailId,
+        password
+      }, {
+        withCredentials: true
+      })
+    } catch (error) {
+      console.error('Error logging in:', error)
+    }
+  }
+
   return (
     <div className="flex justify-center my-10">
-      <h1 className="text-2xl font-bold">Login Page</h1>
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title justify-center text-2xl">Login</h2>
+
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Email ID</legend>
+            <input
+              type="email"
+              className="input w-full"
+              placeholder="Enter your email"
+              value={emailId}
+              onChange={(e) => setEmailId(e.target.value)}
+            />
+          </fieldset>
+
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Password</legend>
+            <input
+              type="password"
+              className="input w-full"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </fieldset>
+
+          <div className="card-actions justify-center mt-4">
+            <button className="btn btn-primary w-full" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
