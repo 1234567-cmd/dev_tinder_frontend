e2c1821661bddcfb6c59449e827375fd02cf8818
waseem ahmad <waseemlone246@gmail.com>
2026-09-14 07:34:49 +0500

    feat: implement user data fetching in Body component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,8 +2,30 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { NavBar } from './NavBar'
 import { Footer } from './Footer'
+import { BASE_URL } from '../utils/constants'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { addUser } from '../utils/userSlice'
 
 export const Body = () => {
+  const dispatch = useDispatch()
+
+  const fetchUserData = async () => {
+    try {
+      const res= await axios.get(`${BASE_URL}/profile/view`, {
+        withCredentials: true
+      })
+      console.log('User data fetched successfully:', res.data)
+      dispatch(addUser(res.data))
+    } catch (error) {
+      console.error('Error fetching user data:', error)
+    }
+  }
+
+  useEffect(() => {
+    fetchUserData()
+  }, [])
   return (
     <div className="flex min-h-screen flex-col">
       <NavBar />
